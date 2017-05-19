00d3bbc48d180e317f18ddc4a80abe1f6549d96a
Yufree <yufree@live.cn>
2017-05-19 19:06:18 -0400

    add

--- R/temp.R
+++ R/temp.R
@@ -92,3 +92,100 @@ getopqedata <- function(path,
         }
         return(xset3)
 }
+
+#' @export
+getopqtofdata <- function(path,
+                          index = F,
+                          xsmethod = "centWave",
+                          peakwidth = c(6.75, 46.5),
+                          ppm = 29,
+                          noise = 0,
+                          snthresh = 10,
+                          mzdiff = 0.00692,
+                          prefilter = c(0, 0),
+                          mzCenterFun = "wMean",
+                          integrate = 1,
+                          fitgauss = FALSE,
+                          verbose.columns = FALSE,
+                          BPPARAM = BiocParallel::SnowParam(workers = 12),
+                          rmethod = "obiwarp",
+                          plottype       = "none",
+                          distFunc       = "cor_opt",
+                          profStep       = 1,
+                          center         = 1,
+                          response       = 1,
+                          gapInit        = 0.48,
+                          gapExtend      = 2.4,
+                          factorDiag     = 2,
+                          factorGap      = 1,
+                          localAlignment = 0,
+                          gmethod = "density",
+                          bw      = 1.74,
+                          mzwid   = 0.01905,
+                          minfrac = 0.5,
+                          minsamp = 1,
+                          gmax     = 20,
+                          lockmass = T,
+                          ...){
+        cdffiles <- list.files(path, recursive = TRUE, full.names = TRUE)
+
+        if (index) {
+                cdffiles <- cdffiles[index]
+        }
+        xset <- xcms::xcmsSet(
+                cdffiles,
+                method = xsmethod,
+                snthresh = snthresh,
+                mzdiff = mzdiff,
+                BPPARAM = BPPARAM,
+                peakwidth = peakwidth,
+                ppm = ppm,
+                noise = noise,
+                prefilter = prefilter,
+                mzCenterFun = mzCenterFun,
+                integrate = integrate,
+                fitgauss = fitgauss,
+                verbose.columns = verbose.columns,
+                lockMassFreq = lockmass,
+                ...
+        )
+        if (index & length(index) == 1) {
+                xset3 <- xset
+        } else{
+                xset <- xcms::group(
+                        xset,
+                        method = gmethod,
+                        bw = bw,
+                        mzwid = mzwid,
+                        minfrac = minfrac,
+                        minsamp = minsamp,
+                        max = gmax
+                )
+                xset2 <- xcms::retcor(
+                        xset,
+                        method = rmethod,
+                        plottype = plottype,
+                        distFunc = distFunc,
+                        profStep = profStep,
+                        center = center,
+                        response = response,
+                        gapInit = gapInit,
+                        gapExtend = gapExtend,
+                        factorDiag = factorDiag,
+                        factorGap = factorGap,
+                        localAlignment = localAlignment
+                )
+                # you need group the peaks again for this corrected data
+                xset2 <- xcms::group(
+                        xset2,
+                        method = gmethod,
+                        bw = bw,
+                        mzwid = mzwid,
+                        minfrac = minfrac,
+                        minsamp = minsamp,
+                        max = gmax
+                )
+                xset3 <- xcms::fillPeaks(xset2, BPPARAM = BPPARAM)
+        }
+        return(xset3)
+}
